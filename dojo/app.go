aceb06aeec847549c7e5659382c25583a3be2697
David J Felix <felix.davidj@gmail.com>
2015-10-19 02:15:35 +0000

    Add some app nonsense and fix the app

--- dojo/app.go
+++ dojo/app.go
@@ -1,7 +1,34 @@
-package dojo
+package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+	"github.com/gorilla/mux"
+	"github.com/codegangsta/cli"
+);
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+
+func webServer(c *cli.Context) {
+	// We're using mux to simplify routing. It's what David knows.
+	router := mux.NewRouter()
+	
+	// Serve the multiplexer
+	http.Handle("/", router)
+	http.ListenAndServe(":5000", nil)
+}	
+
 
 func main() {
-  fmt.Println("I forgot to prep for the dojo. Shame")
+	app := cli.NewApp()
+	app.Name = "dojo"
+	app.Usage = "Run a webserver for the dojo"
+	app.Version = "0.0.1"
+	app.Author = "David J Felix, and the dojo folks"
+	app.Action = webServer
+	app.Flags = []cli.Flag {
+	}
+	app.Run(os.Args)
 }
